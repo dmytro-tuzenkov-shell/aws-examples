13efddf8e04417fc85ec08144ff8e96aa6738da4
Dmytro.Tuzenkov <dmytro.tuzenkov@shell.com>
2023-02-07 19:36:57 +0100

    chore(code): setup typescript+jest

---
+++ sam-app/hello-world/src/index.spec.ts
@@ -0,0 +1,8 @@
+import { describe, expect, test } from "@jest/globals";
+
+describe("sum module", () => {
+  test("adds 1 + 2 to equal 3", () => {
+    expect(1).toBe(1)
+    expect(2).toEqual(2)
+  });
+});
