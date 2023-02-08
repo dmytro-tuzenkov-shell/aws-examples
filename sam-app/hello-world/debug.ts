24040a90c358c97a8beef254df0d519d5a6cfad4
Dmytro.Tuzenkov <dmytro.tuzenkov@shell.com>
2023-02-08 13:27:46 +0100

    chore()

--- sam-app/hello-world/debug.ts
+++ sam-app/hello-world/debug.ts
@@ -1,11 +1,10 @@
-import { bootstrap } from './src/bootstrap';
-import { container } from './src/ioc';
+import "dotenv/config";
+import { bootstrap } from "./src/bootstrap";
+import { container } from "./src/ioc";
 
-;(async () => {
-        
-      const app = bootstrap(container);
-      const { statusCode, json } = await app.handle(null, null);
-    
-      console.log({ statusCode, json });
+(async () => {
+  const app = bootstrap(container);
+  const { statusCode, json } = await app.handle(null, null);
 
-})()
+  console.log({ statusCode, json });
+})();
